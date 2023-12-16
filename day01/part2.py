e105f2829604194d242bf95a00edad6ad4256e56
Christian Kehrmann <christian.kehrmann@advantest.com>
2023-12-16 17:57:36 +0100

    day01, part2

--- day01/part2.py
+++ day01/part2.py
@@ -1,49 +1,57 @@
-#!/usr/bin/env /usr/bin/python36
-import re
-
+#!/usr/bin/env /usr/bin/python
 input_data_file = "part2.data"
 
-
-def extract_number_from_line(line):
-    digits = "".join(c for c in line if c.isdecimal())
-    tens = int(digits[0])
-    ones = int(digits[len(digits) - 1])
-    number = 10 * tens + ones
-    return number
-
-
-def leftmost_literal_number_to_number(line):
-    pos = []
+def get_leftmost_digit_from_line(line):
+    # Walk over the string from left to right.
+    # If the character is a digit, return it as int()
+    # If not, check if this is the start of a written digit. If one is found, return it as int()
+    # If we didn't find any, return -1
     number_words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-    for nummer in number_words:
-        stelle = line.find(nummer)
-        if stelle == -1:
-            stelle = 100000
-        pos.append(stelle)
+    index = 0
+    while index < len(line):
+        # If this is a digit, return it right away
+        if line[index].isdecimal():
+            return int(line[index])
+        # No digit, so let's see if we found a digit as written text
+        text_to_check = line[index:]
+        number = 0
+        while number < 10:
+            if text_to_check.startswith(number_words[number]):
+                return number
+            number += 1
+        index += 1
+    return -1
 
-    first_literal_digit = pos.index(min(pos))
-    line = re.sub(number_words[first_literal_digit], str(first_literal_digit), line)
+def get_rightmost_digit_from_line(line):
+    # Walk over the string from right to left.  The idea is the same as in get_leftmost_digit_from_line()
+    number_words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-    return(line)
+    index = len(line) - 1
+    while index >= 0:
+        if line[index].isdecimal():
+            return int(line[index])
+        text_to_check = line[index:]
+        number = 0
+        while number < 10:
+            if text_to_check.startswith(number_words[number]):
+                return number
+            number += 1
+        index -= 1
+    return -1
 
 
 def main():
-
     # uncomment the next line to read the input data from the test file
-    input_data_file = "part2_test.data"
 
+    # input_data_file = "part2_test.data"
+
+    total = 0
     with open(input_data_file) as given_data:
-        total = 0
         for line in given_data:
-            while True:
-                correct_line = leftmost_literal_number_to_number(line)
-                if line == correct_line:
-                    break
-                line = correct_line
-            number = extract_number_from_line(correct_line)
-
-            total += number
+            zehner = get_leftmost_digit_from_line(line.rstrip())
+            einser = get_rightmost_digit_from_line(line.rstrip())
+            total += 10 * zehner + einser
 
         print("Total: {}".format(total))
 
